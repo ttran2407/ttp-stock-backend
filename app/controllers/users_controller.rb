353f146dc057bae7ebab89f321328910d419e79a
github username <github email address>
2019-08-13 16:46:30 -0400

    set up routes, set up and test users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+    def index 
+        @users = User.all
+        render json: @users
+    end
+
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: @user
+        else 
+            render json: {error: ""}, status: 402
+        end
+    end
+
+    def update
+        @user = User.find(param[:id])
+        @user.update(cash: params[:cash])
+        render json: @user
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :cash)
+    end
+end
